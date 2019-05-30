b30b457dc3dc3e289889f7af2c8bb37a3f12ef7d
Nikolay <ns@digio.ch>
2019-05-30 17:16:53 +0300

    my first commit

---
+++ src/Entity/User.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $role;
+
+    /**
+     * User constructor.
+     * @param $email
+     * @param $password
+     * @param $role
+     */
+    public function __construct($email, $password, $role)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role): void
+    {
+        $this->role = $role;
+    }
+
+}
